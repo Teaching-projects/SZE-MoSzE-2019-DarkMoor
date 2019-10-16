cf4ff9f292b8119a62a7187d8b030c3fc9cc93b3
Madarász Patrik <madarasz.patrik@gmail.com>
2019-10-16 22:53:15 +0200

    Fixed DirNameCheck for MKDir

--- Terminal/MKDir.cpp
+++ Terminal/MKDir.cpp
@@ -2,14 +2,22 @@
 #include "terminal.h"
 #include "Directory.h"
 #include <sstream>
+#include <iostream>
 
 void MKDir::Execute(std::string params)
 {
-	AddDirectory(params);
+	if (AddDirectory(params) == nullptr)
+	{
+		std::cout << "Already exists!\n";
+	}
 }
 Directory* MKDir::AddDirectory(std::string path)
 {
-	return Terminal::GetInstance()->GetActual()->AddDirectory(path);
+	if (Terminal::GetInstance()->GetActual()->GetDirectory(path) == nullptr)
+	{
+		return Terminal::GetInstance()->GetActual()->AddDirectory(path);
+	}
+	return nullptr;
 }
 bool MKDir::ValidateParams(std::string params)
 {
